f2711209538d125d9cdd9944d01426e46e379d70
imerrill9 <ian.merrill9@gmail.com>
2017-09-16 14:31:20 -0600

    Implemented the linear device unlocker.

--- src/UnlockerRace.java
+++ src/UnlockerRace.java
@@ -15,6 +15,7 @@ public class UnlockerRace {
         DeviceShift2 shift2 = new DeviceShift2();
         DeviceShift3 shift3 = new DeviceShift3();
         DeviceNeverShift never = new DeviceNeverShift();
+        SinusoidalDevice sinusoidal = new SinusoidalDevice();
 
         long startTime = System.nanoTime();
         Assert.assertThat(unlocker.unlock(device), Is.is(true));
@@ -23,10 +24,11 @@ public class UnlockerRace {
         Assert.assertThat(unlocker.unlock(shift2), Is.is(true));
         Assert.assertThat(unlocker.unlock(shift3), Is.is(true));
         Assert.assertThat(unlocker.unlock(never), Is.is(true));
+        Assert.assertThat(unlocker.unlock(sinusoidal), Is.is(true));
         long endTime = System.nanoTime();
 
         long duration = (endTime - startTime);
-        System.out.println("FourBitTwoDisclosure finishes unlocking 5 devices in : " + duration + " nano seconds");
+        System.out.println("FourBitTwoDisclosure finishes unlocking 7 devices in : " + duration + " nano seconds");
     }
 
     @Test
@@ -38,6 +40,7 @@ public class UnlockerRace {
         DeviceShift2 shift2 = new DeviceShift2();
         DeviceShift3 shift3 = new DeviceShift3();
         DeviceNeverShift never = new DeviceNeverShift();
+        SinusoidalDevice sinusoidal = new SinusoidalDevice();
 
         long startTime = System.nanoTime();
         Assert.assertThat(unlocker.unlock(device), Is.is(true));
@@ -47,9 +50,35 @@ public class UnlockerRace {
         Assert.assertThat(unlocker.unlock(shift3), Is.is(true));
        // assertThat(unlocker.unlock(never), is(true)); the never shift device will not be unlocked with simple algorithm
         Assert.assertThat(unlocker.unlock(shift1), Is.is(true)); //added another shift1 device for fairness.
+        Assert.assertThat(unlocker.unlock(sinusoidal), Is.is(true));
         long endTime = System.nanoTime();
 
         long duration = (endTime - startTime);
-        System.out.println("SimpleDeviceUnlocker finishes unlocking 5 devices in : " + duration + " nano seconds");
+        System.out.println("SimpleDeviceUnlocker finishes unlocking 7 devices in : " + duration + " nano seconds");
+    }
+
+    @Test
+    public void linearTimer() throws Exception {
+        LinearDeviceUnlocker unlocker = new LinearDeviceUnlocker();
+        Device device = new Device();
+        CustomDevice random = new CustomDevice();
+        DeviceShift1 shift1 = new DeviceShift1();
+        DeviceShift2 shift2 = new DeviceShift2();
+        DeviceShift3 shift3 = new DeviceShift3();
+        DeviceNeverShift never = new DeviceNeverShift();
+        SinusoidalDevice sinusoidal = new SinusoidalDevice();
+
+        long startTime = System.nanoTime();
+        Assert.assertThat(unlocker.unlock(device), Is.is(true));
+        Assert.assertThat(unlocker.unlock(random), Is.is(true));
+        Assert.assertThat(unlocker.unlock(shift1), Is.is(true));
+        Assert.assertThat(unlocker.unlock(shift2), Is.is(true));
+        Assert.assertThat(unlocker.unlock(shift3), Is.is(true));
+        Assert.assertThat(unlocker.unlock(never), Is.is(true));
+        Assert.assertThat(unlocker.unlock(sinusoidal), Is.is(true));
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+        System.out.println("Linear unlocker finishes unlocking 7 devices in : " + duration + " nano seconds");
     }
 }
