b219fc84e94af9d03e3e89423ae7436fe97c7173
Shahid Foy <shahidfoy@gmail.com>
2017-09-16 17:11:48 -0600

    updated javadoc

--- src/FourBitTwoDisclosureDeviceUnlocker.java
+++ src/FourBitTwoDisclosureDeviceUnlocker.java
@@ -25,16 +25,15 @@ import java.util.Random;
             
     } 
 
-
     /**
-     * @param dev the device controlling the resource
-     * to unlock; must be a 4-bit device with 2
-     * peek/poke bits.
-     * @return true if the resource is successfully
-     * unlocked (all bits are now identical);
-     * false otherwise
-     **/
-
+    * Unlocks a resource controlled by a 4-bit/2-disclosure device.
+    * @param dev the device controlling the resource
+    * to unlock; must be a 4-bit device with 2
+    * peek/poke bits.
+    * @return true if the resource is successfully
+    * unlocked (all bits are now identical);
+    * false otherwise
+    **/
     public static boolean unlock(Device dev) {
             int count = 0;
             final int MAX = 100000;
